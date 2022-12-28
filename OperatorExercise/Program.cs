1decac679b32dc30ab3b35146b3d2eb87cb1aaee
Drew Lofland <alofland25@gmail.com>
2022-12-28 16:15:29 -0500

    added a few lines to exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,7 +6,17 @@
         {
             int a = 17;
             int b = 4;
+            
+            int sum = a + b;
+            Console.WriteLine(sum);
+            int diff = a - b;
+            Console.WriteLine(diff);    
+            int product = a * b;
+            Console.WriteLine(product);
+
+
             int quotient = a / b;
+
             int remainder = a % b;
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 
@@ -23,7 +33,7 @@
         {
             var area = Math.PI * Math.Pow(radius, 2);
 
-            return area;
+            return area;    
         }
     }
 }       
